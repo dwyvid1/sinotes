8f566b4b1afdf97b4274056e2f3278e8ca581d96
Deyvid Matheus <deyvid.nascimento0101@gmail.com>
2025-03-11 22:29:09 -0300

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import './App.css'
+import './styles/index.css'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <>
-      <h1>Hello World</h1>  
+      <Header />
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import "../styles/Header.css"
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>S.I Notes</h1>
+      <a href="/sobre">Sobre o projeto</a>
+    </header>
+  );
+}
+
+export default Header;
